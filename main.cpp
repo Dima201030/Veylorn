7370932731bcfe8008721845967370c4c5e3208a
lilnimp <dima@kojem.ru>
2025-12-27 00:01:01 +0300

    All good

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include <cstddef>
 
-// #include "lvlparser.h"
 #include "map.h"
 
 int main() {
-    // test, debug, DEBUG , DEBUG:
-    // std::cout << countColumns("/Users/dima/PycharmProjects/HW/main.txt") << std::endl;
-
-    // std::cout << checkToValidMap("/Users/dima/Veylorn/examples/exampleMap.txt") << std::endl;
 
     Map levelMap("/Users/dima/Veylorn/examples/exampleMap.txt");
 
